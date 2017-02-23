05ec348051235a759b730aace5b0225e6ae7341f
charlotte <charlotte@codingame.com>
2017-02-23 15:59:38 +0100

    part03 step verifier

--- src/test/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/test/java/io/pivotal/literx/Part03StepVerifier.java
@@ -15,7 +15,7 @@
  */
 
 package io.pivotal.literx;
-
+//[imports] { autofold
 import java.time.Duration;
 import java.util.function.Supplier;
 
@@ -25,7 +25,7 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
+//}
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
  *
@@ -44,22 +44,12 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-	@Test
-	public void expect2ElementsThenError() {
-		expectFooBarError(Flux.just("foo", "bar").concatWith(Mono.error(new RuntimeException())));
-	}
-
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
 		fail();
 	}
 
 //========================================================================================
-
-	@Test
-	public void expectElementsWithThenComplete() {
-		expectSkylerJesseComplete(Flux.just(new User("swhite", null, null), new User("jpinkman", null, null)));
-	}
 
 	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite" username and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
@@ -68,22 +58,12 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-	@Test
-	public void count() {
-		expect10Elements(Flux.interval(Duration.ofSeconds(1)).take(10));
-	}
-
 	// TODO Expect 10 elements then complete and notice how long it takes for running the test
 	void expect10Elements(Flux<Long> flux) {
 		fail();
 	}
 
 //========================================================================================
-
-	@Test
-	public void countWithVirtualTime() {
-		expect3600Elements(() -> Flux.interval(Duration.ofSeconds(1)).take(3600));
-	}
 
 	// TODO Expect 3600 elements then complete using the virtual time capabilities provided via StepVerifier.withVirtualTime() and notice how long it takes for running the test
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
